511dafdaf2a9463a1c64bffa1898ccf4151bf026
mt1976 <31077343+mt1976@users.noreply.github.com>
2019-09-18 14:50:47 +0100

    Bum

---
+++ Siena Reset GL DB.sql
@@ -0,0 +1,8 @@
+/****** Script for SelectTopNRows command from SSMS  ******/
+
+TRUNCATE TABLE [SienaGL_demo-sys-3].[dbo].[GATEWAY_CONTROL]
+TRUNCATE TABLE [SienaGL_demo-sys-3].[dbo].[GlLiveDeal]
+TRUNCATE TABLE [SienaGL_demo-sys-3].[dbo].[GlPosting]
+TRUNCATE TABLE [SienaGL_demo-sys-3].[dbo].[GlClosingBalance]
+TRUNCATE TABLE [SienaGL_demo-sys-3].[dbo].[GlMovementProcessed]
+TRUNCATE TABLE [SienaGL_demo-sys-3].[dbo].[GlSystemMessage]
